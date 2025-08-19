df574cbdf1f464f4e2bf7c4b93cbbdc022d077ef
Shravan Seebran <shravanseebran@gmail.com>
2025-08-19 09:52:25 +0200

    chore: initialised backend as uv project

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,6 @@
-from fastapi import FastAPI
-from routes import products, suppliers, transactions, users # Import all routers
-from .database import engine, Base
+def main():
+    print("Hello from backend!")
 
-# Create the database tables
-Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"message": "Inventory Lab API running"}
-
-# Include all the new routers
-app.include_router(products.router)
-app.include_router(suppliers.router)
-app.include_router(transactions.router)
-app.include_router(users.router)
+if __name__ == "__main__":
+    main()
