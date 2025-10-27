a4737f6956d46cfc3df863813597fd17f986802c
Kea-nhlapo <appalingdodge@gmail.com>
2025-10-27 18:02:51 +0200

    add imports for products router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.api.v1.users import router as user_router
-
+from app.api.v1 import products
+app.include_router(products.router, prefix="/api/v1")
 app = FastAPI()
 
 app.include_router(user_router)
@@ -9,3 +10,4 @@ app.include_router(user_router)
 @app.get("/")
 def home():
     return {"message": "hello world!"}
+
